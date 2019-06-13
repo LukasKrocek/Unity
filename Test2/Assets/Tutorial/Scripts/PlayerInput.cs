db5a12debd1b0995e18e1c81e06fcf5eea74c4a2
LukasKrocek <Krocek.Lukas@email.cz>
2019-06-13 10:27:03 +0200

    commiting test projects

---
+++ Test2/Assets/Tutorial/Scripts/PlayerInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerInput : MonoBehaviour {
+
+    private PlayerMovement c_movement;  //Reference to PlayerMovement script
+    private bool isJumping;             //To determine if the player is jumping
+    private bool isWalking;
+	
+	void Awake()
+    {
+        //References
+        c_movement = GetComponent<PlayerMovement>();
+	}
+	
+	void Update ()
+    {
+
+
+        if (!isWalking) { 
+        isWalking = Input.GetKey(KeyCode.LeftShift);
+        }
+        //If he is not jumping...
+        if (!isJumping)
+        {
+            //See if button is pressed...
+            isJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+        }	
+	}
+
+    private void FixedUpdate()
+    {
+        //Get horizontal axis
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        //Call movement function in PlayerMovement
+        c_movement.Move(h, isJumping,isWalking);
+        //Reset
+        isJumping = false;
+        isWalking = false;
+    }
+}
